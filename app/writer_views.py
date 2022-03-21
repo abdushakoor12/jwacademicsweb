e9608134532a40120aee042b49e318f0909fbf95
root <root@production.production>
2022-03-21 16:33:47 +0800

    Added Flask Library with Main Function for WSGI Serve

--- app/writer_views.py
+++ app/writer_views.py
@@ -1,6 +1,11 @@
-from app import app
-from flask import render_template
+#from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route('/writer/home')
 def writer_home():
 	return "Writer Home"
+
+if __name__ == '__main__':
+	app.run()
