c4b03e340322975970ac6b7738c9dd7a53c9c95c
root <root@production.production>
2022-03-21 16:31:46 +0800

    Added Flask Library with Main Function for WSGI Serve

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,6 +1,12 @@
-from app import app
-from flask import render_template
+#from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route('/admin/home')
 def admin_home():
 	return "Admin Home"
+
+if __name__ == '__main__':
+	app.run()
