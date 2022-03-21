49743133f40e07913babdf87a8523fd909edbde9
root <root@production.production>
2022-03-21 16:32:32 +0800

    Added Flask Library with Main Function for WSGI Server

--- app/user_views.py
+++ app/user_views.py
@@ -1,6 +1,13 @@
-from app import app
-from flask import render_template
+#from app import app
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
 
 @app.route('/user/home')
 def user_home():
 	return "Welcome User"
+
+
+if __name__ == '__main__':
+	app.run()
